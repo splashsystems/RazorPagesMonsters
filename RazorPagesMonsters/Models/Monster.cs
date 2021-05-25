085fe9dd4c2adbb005e00a2f1c6cba65d0d23454
Scot <bscot.hagen@gmail.com>
2021-05-25 12:22:10 -0700

    added Seed Data

--- RazorPagesMonsters/Models/Monster.cs
+++ RazorPagesMonsters/Models/Monster.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,8 +12,10 @@ namespace RazorPagesMonsters.Models
         public int ID { get; set; }
         public string MonsterName { get; set; }
         [DataType(DataType.Date)]
+        [Display(Name = "Date Introduced")]
         public DateTime YearPopularized { get; set; }
         public string Genre { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
         public Decimal Price { get; set; }
     }
 }
---
+++ RazorPagesMonsters/Models/SeedData.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RazorPagesMonsters.Data;
+
+namespace RazorPagesMonsters.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new RazorPagesMonstersContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<RazorPagesMonstersContext>>()))
+            {
+                // Look for any Monsters.
+                if (context.Monster.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Monster.AddRange(
+                    new Monster
+                    {
+                        MonsterName = "When Harry Met Sally",
+                        YearPopularized = DateTime.Parse("1989-2-12"),
+                        Genre = "Romantic Comedy",
+                        Price = 7.99M
+                    },
+
+                    new Monster
+                    {
+                        MonsterName = "Pikachu",
+                        YearPopularized = DateTime.Parse("1996-2-27"),
+                        Genre = "Pokemon",
+                        Price = 5.67M
+                    },
+
+                    new Monster
+                    {
+                        MonsterName = "Slimer",
+                        YearPopularized = DateTime.Parse("1984-6-15"),
+                        Genre = "Ghostbusters",
+                        Price = 34.55M
+                    },
+
+                    new Monster
+                    {
+                        MonsterName = "Alien",
+                        YearPopularized = DateTime.Parse("1979-5-25"),
+                        Genre = "Space Monsters",
+                        Price = 10.00M
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
